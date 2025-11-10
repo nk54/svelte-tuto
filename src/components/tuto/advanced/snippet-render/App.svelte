8205bbdf1889874b3d46f83ba0d83602e9c7dc03
nk54 <nk54@outlook.com>
2025-11-11 00:20:25 +0100

    snippet render

--- src/components/tuto/advanced/snippet-render/App.svelte
+++ src/components/tuto/advanced/snippet-render/App.svelte
@@ -1,2 +1,44 @@
 <script lang="ts">
 </script>
+
+<table>
+  <thead>
+    <tr>
+      <th>emoji</th>
+      <th>description</th>
+      <th>unicode escape sequence</th>
+      <th>html entity</th>
+    </tr>
+  </thead>
+
+  <tbody>
+    {#snippet monkey(emoji: string, description: string)}
+      <tr>
+        <td>{emoji}</td>
+        <td>{description}</td>
+        <td
+          >\u{emoji.charCodeAt(0).toString(16)}\u{emoji
+            .charCodeAt(1)
+            .toString(16)}</td
+        >
+        <td>&amp#{emoji.codePointAt(0)}</td>
+      </tr>
+    {/snippet}
+    {@render monkey("🙈", "see no evil")}
+    {@render monkey("🙉", "hear no evil")}
+    {@render monkey("🙊", "speak no evil")}
+  </tbody>
+</table>
+
+<style>
+  th,
+  td {
+    padding: 0.5em;
+    text-align: center;
+  }
+
+  td:nth-child(3),
+  td:nth-child(4) {
+    font-family: monospace;
+  }
+</style>
