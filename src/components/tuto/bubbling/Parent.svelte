a5fb933fe615cadeb682c368b27054485eea932d
nk54 <nk54@outlook.com>
2025-11-10 10:37:01 +0100

    bubbling

---
+++ src/components/tuto/bubbling/Parent.svelte
@@ -0,0 +1,19 @@
+<div onkeydown={(e) => alert(`<div> ${e.key}`)} role="presentation">
+  <span>From child to parent (input > div) : onkeydown</span>
+  <input onkeydown={(e) => alert(`<input> ${e.key}`)} />
+</div>
+
+<div onkeydowncapture={(e) => alert(`<div> ${e.key}`)} role="presentation">
+  <span>From parent to child (div > parent) : onkeydowncapture</span>
+  <input onkeydowncapture={(e) => alert(`<input> ${e.key}`)} />
+</div>
+
+<style>
+  @reference "@/styles/global.css";
+  div {
+    @apply flex flex-col gap-2 mb-4;
+  }
+  input {
+    @apply border;
+  }
+</style>
