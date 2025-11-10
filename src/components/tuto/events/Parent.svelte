30312c064f4e7d0518b549760518eb3d200102c8
nk54 <nk54@outlook.com>
2025-11-10 10:31:22 +0100

    events

---
+++ src/components/tuto/events/Parent.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  let mousePosition = $state({ x: 0, y: 0 });
+
+  function onpointermove(event: PointerEvent) {
+    mousePosition.x = event.clientX;
+    mousePosition.y = event.clientY;
+  }
+</script>
+
+<div {onpointermove} class="flex-1">
+  The pointer is at {Math.round(mousePosition.x)} x {Math.round(
+    mousePosition.y
+  )}
+</div>
