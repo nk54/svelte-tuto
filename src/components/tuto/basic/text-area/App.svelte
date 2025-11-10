0f38033a157624326a5e61700535bc0e360712d1
nk54 <nk54@outlook.com>
2025-11-10 14:13:49 +0100

    text area markdown

---
+++ src/components/tuto/basic/text-area/App.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { marked } from "marked";
+
+  let value = $state(
+    `Some words are *italic*, some are **bold**\n\n- lists\n- are\n- cool`
+  );
+</script>
+
+<div class="gap-4 grid grid-cols-[5em_1fr] grid-rows-2 h-full">
+  input
+  <textarea bind:value></textarea>
+
+  output
+  <div class="prose">{@html marked(value)}</div>
+</div>
