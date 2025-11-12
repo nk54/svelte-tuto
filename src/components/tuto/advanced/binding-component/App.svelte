332f3b89fdfc50f2106d768f81d86ce41fbe20b0
nk54 <nk54@outlook.com>
2025-11-12 12:33:04 +0100

    binding component

--- src/components/tuto/advanced/binding-component/App.svelte
+++ src/components/tuto/advanced/binding-component/App.svelte
@@ -1,4 +1,19 @@
 <script lang="ts">
+  import { Keypad } from "./";
+
+  let pin = $state("");
+
+  let view = $derived(
+    pin ? pin.toString().replace(/\d(?!$)/g, "•") : "enter your pin"
+  );
+
+  function onsubmit() {
+    alert(`submitted ${pin}`);
+  }
 </script>
 
-Hi
+<h1 style="opacity: {pin ? 1 : 0.4}">
+  {view}
+</h1>
+
+<Keypad bind:value={pin} {onsubmit} />
