8ff1fc175e5c18af3eae00cc235da8236c923454
nk54 <nk54@outlook.com>
2025-11-12 17:19:11 +0100

    svelte boundary : flaky component

---
+++ src/components/tuto/special/svelte-boundary/FlakyComponent.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  let mouse = $state({ x: 0, y: 0 });
+</script>
+
+<svelte:window
+  onmousemove={(e) => {
+    mouse.x = e.clientX;
+    mouse.y = e.clientY;
+  }}
+/>
+
+<p>{mouse.x}x{mouse.y}</p>
+
+<button onclick={() => (mouse = null!)} class="bg-red-500 px-4 py-2 border rounded text-white">
+  whatever you do, don't click this button
+</button>
