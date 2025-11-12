56b64089465e2fddf41d8a47b90795920c33c9a2
nk54 <nk54@outlook.com>
2025-11-12 13:54:02 +0100

    transition deferred

---
+++ src/components/tuto/advanced/transition-deferred/TodoList.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+  import { type TodoItem, send, receive } from "./";
+  let {
+    todos,
+    remove,
+  }: { todos: TodoItem[]; remove: (todo: TodoItem) => void } = $props();
+</script>
+
+<ul class="todos">
+  {#each todos as todo (todo.id)}
+    <li
+      class={todo.done ? "done" : "todo"}
+      in:receive={{ key: todo.id }}
+      out:send={{ key: todo.id }}
+    >
+      <label>
+        <input type="checkbox" bind:checked={todo.done} />
+        <span>{todo.description}</span>
+        <button onclick={() => remove(todo)} aria-label="Remove"></button>
+      </label>
+    </li>
+  {/each}
+</ul>
+
+<style>
+  label {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    gap: 0.5rem;
+  }
+
+  span {
+    flex: 1;
+  }
+
+  button {
+    background-image: url(./remove.svg);
+  }
+  .todo {
+    color: red;
+  }
+  .done {
+    color: green;
+  }
+</style>
